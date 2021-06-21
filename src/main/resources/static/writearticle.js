c7b090acf6d53c8b1cb7404e4ea465c16f12c93c
novb1492 <novb1492@naver.com>
2021-06-21 13:45:57 +0900

    --

--- src/main/resources/static/writearticle.js
+++ src/main/resources/static/writearticle.js
@@ -4,4 +4,14 @@ function doInsertArticle(){
     var xhr = new XMLHttpRequest();
     xhr.open("POST" , "/insertarticle" , true);
     xhr.send(formData);
+    xhr.onload = function() { 
+        if(xhr.status==200){ // success:function(data)부분 통신 성공시 200반환
+            if(xhr.response=='true'){
+                alert("작성완료");
+                location.href="/auth/boardlist";
+            }else{
+                alert("등록에 실패했습니다");
+            }
+        }
+    }  
  }
